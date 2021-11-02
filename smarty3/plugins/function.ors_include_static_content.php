68cce6956644a7a2f4bbd1de6905f408d4b1263f
Frank Barthold <f.barthold@mertens.ag>
2021-11-02 09:08:58 +0100

    Eigene Smarty-Funktion zum Einbetten von textfiles (z.B. bestellung_hinweis.html)

--- smarty3/plugins/function.ors_include_static_content.php
+++ smarty3/plugins/function.ors_include_static_content.php
@@ -1,7 +1,20 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: f.barthold
- * Date: 01.11.2021
- * Time: 16:48
- */
+function smarty_function_ors_include_static_content($params, &$smarty) {
+
+    if(!isset($params['file'])) {
+        $smarty->trigger_error("gravatar: no 'file' attribute passed");
+        return;
+    }
+
+    $dir = $params['dir'] ?? 'textfiles';
+    $file = $params['file'];
+
+    $appdir = dirname(dirname(__DIR__));
+
+    $filePath = $appdir . '/' . basename($dir) . '/'. $file;
+    if (file_exists($filePath)) {
+        return file_get_contents($filePath);
+    } else {
+        return 'File ' . $file . ' not found in ' . $dir . '!';
+    }
+}
