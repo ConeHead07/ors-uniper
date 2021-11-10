74488067898aa6e7a22019a017095d2e16e0650a
Frank Barthold <f.barthold@mertens.ag>
2021-11-10 17:49:25 +0100

    mertens Auftragsübersicht mit Auftragssummen

--- sites/admin_antraege.php
+++ sites/admin_antraege.php
@@ -1,20 +1,20 @@
 <?php 
 
-if (strpos($user["gruppe"], "admin") === false && $user['gruppe'] !== 'umzugsteam') {
+if (strpos($user['gruppe'], 'admin') === false && $user['gruppe'] !== 'umzugsteam') {
     die("UNERLAUBTER ZUGRIFF!");
 }
 
 
 require_once($InclBaseDir."umzugsantrag.inc.php");
 require_once($InclBaseDir."umzugsmitarbeiter.inc.php");
-$CUA = &$_CONF["umzugsantrag"];
-$CUM = &$_CONF["umzugsmitarbeiter"];
+$CUA = &$_CONF['umzugsantrag'];
+$CUM = &$_CONF['umzugsmitarbeiter'];
 $Tpl = new myTplEngine();
 $Umzuege = array();
 
 
 if (empty($s)) {
-    $s = getRequest("s", "");
+    $s = getRequest('s', "");
 }
 
 $userGruppe = $user['gruppe'];
@@ -45,41 +45,47 @@ if (!$istUmzugsteam) {
 
 $defaultOrder = "ORDER BY antragsdatum ASC";
 $orderFields = array(
-	"id" => array("field"=>"U.aid", "defaultOrder"=>"ASC"),
-	"termin" => array("field"=>"umzugstermin", "defaultOrder"=>"ASC"),
-	"von" => array("field"=>"M.gebaeude", "defaultOrder"=>"ASC"),
-	"nach" => array("field"=>"M.ziel_gebaeude", "defaultOrder"=>"ASC"),
-    "ort" => array("field"=>"ort", "defaultOrder"=>"ASC"),
-    "kid" => array("field"=>"user.personalnr", "defaultOrder"=>"ASC"),
-	"umzug" => array("field"=>"umzug", "defaultOrder"=>"ASC"),
-	"mitarbeiter" => array("field"=>"mitarbeiter_num", "defaultOrder"=>"ASC"),
-	"antragsdatum" => array("field"=>"antragsdatum", "defaultOrder"=>"ASC"),
-	"geprueft" => array("field"=>"geprueft", "defaultOrder"=>"ASC"),
-	"genehmigt" => array("field"=>"genehmigt_br", "defaultOrder"=>"ASC"),
-	"bestaetigt" => array("field"=>"bestaetigt", "defaultOrder"=>"ASC"),
-	"abgeschlossen" => array("field"=>"abgeschlossen", "defaultOrder"=>"ASC"),
+	'id' => array('field'=>"U.aid", 'defaultOrder'=>'ASC'),
+	'termin' => array('field'=>'umzugstermin', 'defaultOrder'=>'ASC'),
+	'von' => array('field'=>"M.gebaeude", 'defaultOrder'=>'ASC'),
+	'nach' => array('field'=>"M.ziel_gebaeude", 'defaultOrder'=>'ASC'),
+    'ort' => array('field'=>'ort', 'defaultOrder'=>'ASC'),
+    'kid' => array('field'=>"user.personalnr", 'defaultOrder'=>'ASC'),
+	'umzug' => array('field'=>'umzug', 'defaultOrder'=>'ASC'),
+	'mitarbeiter' => array('field'=>'mitarbeiter_num', 'defaultOrder'=>'ASC'),
+	'antragsdatum' => array('field'=>'antragsdatum', 'defaultOrder'=>'ASC'),
+	'geprueft' => array('field'=>'geprueft', 'defaultOrder'=>'ASC'),
+	'genehmigt' => array('field'=>'genehmigt_br', 'defaultOrder'=>'ASC'),
+	'bestaetigt' => array('field'=>'bestaetigt', 'defaultOrder'=>'ASC'),
+	'abgeschlossen' => array('field'=>'abgeschlossen', 'defaultOrder'=>'ASC'),
 );
 if ($ofld && isset($orderFields[$ofld])) {
-	$orderBy = "ORDER BY ".$orderFields[$ofld]["field"]." ";
-	$orderBy.= ($odir) ? ($odir!="DESC" ? "ASC" : "DESC") : $orderFields[$ofld]["defaultOrder"];
+	$orderBy = "ORDER BY ".$orderFields[$ofld]['field']." ";
+	$orderBy.= ($odir) ? ($odir!='DESC' ? 'ASC' : 'DESC') : $orderFields[$ofld]['defaultOrder'];
 } else {
 	$orderBy = $defaultOrder;
 }
 
-$ListBaseLink = "?s=".urlencode($s)."&cat=".urlencode($cat).($allusers ? '&allusers=1' : '');
+$ListBaseLink = '?s='.urlencode($s).'&cat='.urlencode($cat).($allusers ? '&allusers=1' : '');
 
-$sqlFrom  = "FROM `".$CUA["Table"]."` U LEFT JOIN `".$CUM["Table"]."` M USING(aid)\n"
-           ." LEFT JOIN mm_user user ON U.antragsteller_uid = user.uid \n "
-           ." LEFT JOIN mm_stamm_gebaeude g  ON U.gebaeude = g.id \n"
-           ." LEFT JOIN mm_stamm_gebaeude vg ON U.von_gebaeude_id = vg.id \n"
-           ." LEFT JOIN mm_stamm_gebaeude ng ON U.nach_gebaeude_id = ng.id \n";
+$sqlFrom  = "FROM `".$CUA['Table']."` U LEFT JOIN `".$CUM['Table']."` M USING(aid)\n"
+           . " LEFT JOIN mm_user user ON U.antragsteller_uid = user.uid \n "
+           . " LEFT JOIN mm_stamm_gebaeude g  ON U.gebaeude = g.id \n"
+           . " LEFT JOIN mm_stamm_gebaeude vg ON U.von_gebaeude_id = vg.id \n"
+           . " LEFT JOIN mm_stamm_gebaeude ng ON U.nach_gebaeude_id = ng.id \n"
+           . ' LEFT JOIN mm_umzuege_leistungen ul ON (U.aid = ul.aid) ' . "\n"
+           . ' LEFT JOIN mm_leistungskatalog l ON(ul.leistung_id = l.leistung_id) ' . "\n"
+           . ' LEFT JOIN mm_leistungspreismatrix lm ON(' . "\n"
+           . '    l.leistung_id = lm.leistung_id ' . "\n"
+           . '    AND lm.mengen_von <= (ul.menge_mertens * IFNULL(ul.menge2_mertens,1)) ' . "\n"
+           . '    AND (lm.mengen_bis >= ( ul.menge_mertens * IFNULL(ul.menge2_mertens,1)))' . "\n"
+           . ' ) ' . "\n";
 $sqlWhere = "WHERE 1\n";
 
-
 if (!$allusers) {
-    if ($user["gruppe"]=="admin_standort") {
+    if ($user['gruppe']=='admin_standort') {
         $sqlWhere.= "AND (";
-        $sqlWhere.= "    U.ort IN (\"".str_replace(",", "\",\"", $user["standortverwaltung"])."\")\n";
+        $sqlWhere.= "    U.ort IN (\"".str_replace(",", "\",\"", $user['standortverwaltung'])."\")\n";
         $sqlWhere.= '    OR g.mertenshenk_uid = ' . (int)$user['uid'] . ' ';
         $sqlWhere.= '    OR U.antragsteller_uid = ' . (int)$user['uid'] . ' ';
         $sqlWhere.= ' )';
@@ -90,49 +96,49 @@ if (!$allusers) {
 }
 
 switch($cat) {
-	case "neue":
+	case 'neue':
 	$sqlWhere.= "AND umzugsstatus IN ('angeboten', 'beantragt', 'erneutpruefen')\n";
 	break;
     
-	case "angeboten":
+	case 'angeboten':
 	$sqlWhere.= "AND (umzugsstatus = 'angeboten' or umzugsstatus='geprueft' AND umzug='Ja')\n";
 	break;
 	
-	case "gepruefte":
+	case 'gepruefte':
 	$sqlWhere.= "AND umzugsstatus = 'geprueft'\n";
 	break;
 	
-	case "genehmigte":
+	case 'genehmigte':
 	$sqlWhere.= "AND umzugsstatus = 'genehmigt'\n";
 	break;
 
-    case "heute":
+    case 'heute':
         $sqlWhere.= "AND DATE_FORMAT(umzugstermin, '%Y-%m-%d') = '" . date('Y-m-d') . "'";
         $sqlWhere.= "AND (umzugsstatus IN ('geprueft', 'bestaetigt','genehmigt') OR (umzug='Nein' AND umzugsstatus='angeboten'))\n";
         break;
 
-    case "aktive":
+    case 'aktive':
         $sqlWhere.= "AND (umzugsstatus IN ('geprueft', 'bestaetigt','genehmigt') OR (umzug='Nein' AND umzugsstatus='angeboten'))\n";
         break;
 	
-	case "abgeschlossene":
+	case 'abgeschlossene':
 	$sqlWhere.= "AND (umzugsstatus = 'abgeschlossen' AND abgeschlossen = 'Ja')\n";
 	//$sqlWhere.= "OR (abgeschlossen !=  'Init' AND  abgeschlossen IS NOT NULL)) \n";
 	break;
 	
-	case "abgelehnte":
+	case 'abgelehnte':
 	$sqlWhere.= "AND (umzugsstatus = 'abgelehnt')\n";
 	break;
 	
-	case "temp":
+	case 'temp':
 	$sqlWhere.= "AND umzugsstatus IN ('temp','zurueckgegeben')\n";
 	break;
 	
-	case "zurueckgegeben":
+	case 'zurueckgegeben':
 	$sqlWhere.= "AND umzugsstatus = 'zurueckgegeben'\n";
 	break;
 	
-	case "stornierte":
+	case 'stornierte':
 	$sqlWhere.= "AND (abgeschlossen = 'Storniert' OR umzugsstatus = 'storniert')\n";
 	break;
 }
@@ -142,9 +148,10 @@ $sql.= $sqlFrom.$sqlWhere;
 //$sql.= "GROUP BY aid\n";
 $row = $db->query_singlerow($sql);
 //echo $db->error()."<br>\nsql: $sql <br>\n";
-$num_all = $row["count"];
+$num_all = $row['count'];
 
-$sql = 'SELECT U.*, user.personalnr AS kid, CONCAT(vg.stadtname, " ", vg.adresse) gebaeude, CONCAT(ng.stadtname, " ", ng.adresse) ziel_gebaeude ' . PHP_EOL;
+$sql = 'SELECT U.*, user.personalnr AS kid, CONCAT(vg.stadtname, " ", vg.adresse) von_gebaeude, CONCAT(ng.stadtname, " ", ng.adresse) ziel_gebaeude ' . PHP_EOL;
+$sql.= ', SUM(if(lm.preis, lm.preis, preis_pro_einheit) * ul.menge_mertens * IFNULL(ul.menge2_mertens,1)) AS summe' . "\n";
 $sql.= $sqlFrom.$sqlWhere;
 $sql.= "GROUP BY aid\n";
 $sql.= $orderBy."\n";
@@ -153,15 +160,20 @@ $all = $db->query_rows($sql);
 // echo $db->error()."<br>\nsql: $sql <br>\n";
 $num = count($all);
 
+$sqlSummeTotal = 'SELECT SUM(summe) FROM (';
+$sqlSummeTotal.= substr($sql, 0, strpos($sql, "\nLIMIT "));
+$sqlSummeTotal.= ') AS SummenTable';
+$summeTotal = $db->query_one($sqlSummeTotal);
+
 if ($num_all > $num) {
 	$rlist_nav = new listbrowser(array(
-		"offset"     => $offset,
-		"limit"      => $limit,
-		"num_result" => $num,
-		"num_all"    => $num_all,
-		"baselink"   => $ListBaseLink."&offset={offset}&limit={limit}&ofld=$ofld&odir=$odir"));
+		'offset'     => $offset,
+		'limit'      => $limit,
+		'num_result' => $num,
+		'num_all'    => $num_all,
+		'baselink'   => $ListBaseLink."&offset={offset}&limit={limit}&ofld=$ofld&odir=$odir"));
 	$rlist_nav->render_browser();
-	$ListBrowsing = $rlist_nav->get_nav("all")." num:$num num_all:$num_all<br>\n";
+	$ListBrowsing = $rlist_nav->get_nav('all')." num:$num num_all:$num_all<br>\n";
 } else {
 	$ListBrowsing = ""; 
 }
@@ -171,55 +183,59 @@ if ($showSQL) {
 <pre style='background-color: #c9c9c9;color: #626262;padding:.8rem;'>" . $sql . "</pre>Num-Result: " . count($all) . "</div>" . $ListBrowsing;
 }
 
-if (!function_exists("get_iconStatus")) { function get_iconStatus($statVal, $date, $von ='', $statKey ='') {
+if (!function_exists('get_iconStatus')) { 
+    function get_iconStatus($statVal, $date, $von ='', $statKey ='') {
         $alt = '';
         $alt.= (strtotime($date) ? date('d.m H:i', strtotime($date)) : $date);
         if ($statKey) $alt.= ' ' . $statKey . '(' . $statVal . ')';
         if ($von) $alt.= ' von ' . $von;
 	switch(strtoupper($statVal)) {
-		case "JA": return "<img src=\"images/status_ja.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($alt)."\">";
-		case "NEIN": return "<img src=\"images/status_nein.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($alt)."\">";
-		case "INIT": return "<img src=\"images/status_init.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($statVal)."\">";
-		case "STORNIERT": return "<img src=\"images/status_storniert.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($alt)."\">";
-		case "WARNUNG": return "<img src=\"images/warning_triangle.png\" width=\"16\" height=\"16\" alt=\"".fb_htmlEntities($alt)."\">";
+		case 'JA': return "<img src=\"images/status_ja.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($alt)."\">";
+		case 'NEIN': return "<img src=\"images/status_nein.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($alt)."\">";
+		case 'INIT': return "<img src=\"images/status_init.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($statVal)."\">";
+		case 'STORNIERT': return "<img src=\"images/status_storniert.png\" width=\"16\" height=\"16\" title=\"".fb_htmlEntities($alt)."\">";
+		case 'WARNUNG': return "<img src=\"images/warning_triangle.png\" width=\"16\" height=\"16\" alt=\"".fb_htmlEntities($alt)."\">";
 	}
-	return "<span class=\"status".$statVal."\" title=\"".fb_htmlEntities($alt)."\">$statVal</span>";
+	return '<span class="status' . $statVal.'" title="'.fb_htmlEntities($alt).'">' . $statVal . '</span>';
 }}
 
 
 //echo MyDB::error()."<br>$sql<br> num_rows:".count($all).":".print_r($all,1)."\n";
-if (is_array($all)) foreach($all as $i => $item) {
-	$Umzuege[$i] = $item;
+if (is_array($all)) {
+    foreach($all as $i => $item) {
+        $Umzuege[$i] = $item;
 
-	$Umzuege[$i]["LinkOpen"] = "?s=aantrag" . "&id=" . $item["aid"];
-    if ($istUmzugsteam) {
-        $Umzuege[$i]["LinkOpen"] = "?s=aantrag" . "&id=" . $item["aid"] . '&top=' . $s;
+        $Umzuege[$i]['LinkOpen'] = '?s=aantrag' . '&id=' . $item['aid'];
+        if ($istUmzugsteam) {
+            $Umzuege[$i]['LinkOpen'] = '?s=aantrag' . '&id=' . $item['aid'] . '&top=' . $s;
+        }
+        $Umzuege[$i]['Mitarbeiter'] = $item['mitarbeiter_num'];
+        $Umzuege[$i]['plz'] = $item['plz']."&nbsp;";
+        $Umzuege[$i]['Von'] = $item['gebaeude']."&nbsp;";
+        $Umzuege[$i]['Nach'] = $item['ziel_gebaeude']."&nbsp;";
+        $Umzuege[$i]['Antragsstatus'] =  $item['antragsstatus'];
+        $Umzuege[$i]['Termin'] = ($item['umzugstermin']?$item['umzugstermin']:$item['terminwunsch']);
+        $Umzuege[$i]['Antragsdatum'] = $item['antragsdatum'];
+
+        $Umzuege[$i]['Avisiert']   = get_iconStatus($item['bestaetigt'], $item['geprueft_am'], $item['geprueft_von'], 'Avisiert');
+        $Umzuege[$i]['Geprueft']   = get_iconStatus($item['geprueft'], $item['geprueft_am'], $item['geprueft_von'], 'Geprueft');
+        $Umzuege[$i]['Genehmigt']  = get_iconStatus($item['genehmigt_br'], $item['genehmigt_br_am'], $item['genehmigt_br_von']);
+        $Umzuege[$i]['Bestaetigt'] = (empty($item['angeboten_am']) ? get_iconStatus($item['genehmigt_br'], $item['genehmigt_br_am']) : '');
+        $Umzuege[$i]['Abgeschlossen'] = get_iconStatus($item['abgeschlossen'], $item['abgeschlossen_am'], $item['abgeschlossen_von']);
     }
-	$Umzuege[$i]["Mitarbeiter"] = $item["mitarbeiter_num"];
-    $Umzuege[$i]["plz"] = $item["plz"]."&nbsp;";
-	$Umzuege[$i]["Von"] = $item["gebaeude"]."&nbsp;";
-	$Umzuege[$i]["Nach"] = $item["ziel_gebaeude"]."&nbsp;";
-	$Umzuege[$i]["Antragsstatus"] =  $item["antragsstatus"];
-	$Umzuege[$i]["Termin"] = ($item["umzugstermin"]?$item["umzugstermin"]:$item["terminwunsch"]);
-	$Umzuege[$i]["Antragsdatum"] = $item["antragsdatum"];
-
-    $Umzuege[$i]["Avisiert"]   = get_iconStatus($item["bestaetigt"], $item["geprueft_am"], $item["geprueft_von"], 'Avisiert');
-    $Umzuege[$i]["Geprueft"]   = get_iconStatus($item["geprueft"], $item["geprueft_am"], $item["geprueft_von"], 'Geprueft');
-	$Umzuege[$i]["Genehmigt"]  = get_iconStatus($item["genehmigt_br"], $item["genehmigt_br_am"], $item["genehmigt_br_von"]);
-	$Umzuege[$i]["Bestaetigt"] = (empty($item['angeboten_am']) ? get_iconStatus($item["genehmigt_br"], $item["genehmigt_br_am"]) : '');
-	$Umzuege[$i]["Abgeschlossen"] = get_iconStatus($item["abgeschlossen"], $item["abgeschlossen_am"], $item["abgeschlossen_von"]);
 }
 
-$Tpl->assign("s", $s);
-$Tpl->assign("cat", $cat);
-$Tpl->assign("allusers", $allusers);
-$Tpl->assign("ListBrowsing", $ListBrowsing);
-$Tpl->assign("ListBaseLink", $ListBaseLink);
-$Tpl->assign("ofld", $ofld);
-$Tpl->assign("odir", $odir);
-$Tpl->assign("num_all", $num_all);
+$Tpl->assign('s', $s);
+$Tpl->assign('cat', $cat);
+$Tpl->assign('allusers', $allusers);
+$Tpl->assign('ListBrowsing', $ListBrowsing);
+$Tpl->assign('ListBaseLink', $ListBaseLink);
+$Tpl->assign('ofld', $ofld);
+$Tpl->assign('odir', $odir);
+$Tpl->assign('num_all', $num_all);
+$Tpl->assign('summeTotal', $summeTotal);
 
-$Tpl->assign("Umzuege", $Umzuege);
+$Tpl->assign('Umzuege', $Umzuege);
 
 //echo '<pre>#' . __LINE__ . ' '; // . print_r( filestat('html/antraege_liste.html'),1);
 try {
