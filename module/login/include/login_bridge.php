6c67199499bb9970ee728f15a6ebaed83b652927
Frank Barthold <f.barthold@mertens.ag>
2021-11-29 10:22:24 +0100

    Fix Default Redirect after Login: Route by User-Gruppe

--- module/login/include/login_bridge.php
+++ module/login/include/login_bridge.php
@@ -1,61 +1,105 @@
 <?php
 
 define("SET_NEW_VISIT", true);
-if (function_exists("db_auto_connect")) db_auto_connect();
+if (function_exists("db_auto_connect")) {
+    db_auto_connect();
+}
+
 // REDIRECT
-if (!isset($msgID)) $msgID=0;
+if (!isset($msgID)) {
+    $msgID=0;
+}
+
 if (empty($redirect)) {
-    if (!empty($_POST["redirect"])   && trim($_POST["redirect"]) ) $redirect = $_POST["redirect"];
-    elseif (!empty($_GET["redirect"]) && trim($_GET["redirect"]) ) $redirect = $_GET["redirect"];
-    else $redirect = $_CONF["redirectAfterLogin"];
+    if (!empty($_POST['redirect'])   && trim($_POST['redirect']) ) {
+        $redirect = $_POST['redirect'];
+    }
+    elseif (!empty($_GET['redirect']) && trim($_GET['redirect']) ) {
+        $redirect = $_GET['redirect'];
+    }
+    else {
+        $redirect = $_CONF['redirectAfterLogin'];
+    }
 }
 
 $m = __LINE__;
-$l = "";
-$content = "";
+$l = '';
+$content = '';
 $show_form = true;
 
 
 // PRUEFE ZUGANGSDATEN
-if (isset($_POST["username"])  && !empty($_POST["username"]) && 
-isset($_POST["password"]) && !empty($_POST["password"]))
+if (isset($_POST['username'])  && !empty($_POST['username']) && 
+isset($_POST['password']) && !empty($_POST['password']))
 {
 	// die("#".__LINE__." _SESSION:".print_r($_SESSION, true)."<br>\n");
 	$m = __LINE__;
-	$l = "n:".$_POST["username"].", p:".$_POST["password"]."; md5(p):".md5($_POST["password"]);
-	$user = get_userByLogin($_POST["username"], md5($_POST["password"]), $m);
+	$l = "n:" . $_POST['username'] . ', p:' . $_POST['password'] . '; md5(p):' . md5($_POST['password']);
+	$user = get_userByLogin($_POST['username'], md5($_POST['password']), $m);
 	
-	if (isset($user["uid"])) {
+	if (isset($user['uid'])) {
 		
 		$m = __LINE__;
-		if ($user["freigegeben"] == "Ja") {
+		if ($user['freigegeben'] == 'Ja') {
 			$m = __LINE__;
-			$test = "test";
+			$test = 'test';
 	        if (function_exists("session_start")) {
 				$m = __LINE__;
-				$SESS_CName = $_CONF["CAuthSessionName"];
+				$SESS_CName = $_CONF['CAuthSessionName'];
 	            
-				$_SESSION[$_CONF["CAuthSessionName"]]["username"] = $_POST["username"];
-	            $_SESSION[$_CONF["CAuthSessionName"]]["password"] = md5($_POST["password"]);
-	            $_SESSION[$_CONF["CAuthSessionName"]]["uid"] = $user["uid"];
-	            $_SESSION[$_CONF["CAuthSessionName"]]["remote_addr"]  = $_SERVER["REMOTE_ADDR"];
-	            $_SESSION[$_CONF["CAuthSessionName"]]["session_name"] = session_name();
-	            $_SESSION[$_CONF["CAuthSessionName"]]["user_agent"]   = $_SERVER["HTTP_USER_AGENT"];
-	            if (!isset($_SESSION["REMOTE_ADDR"])) $_SESSION["REMOTE_ADDR"] = $_SERVER["REMOTE_ADDR"];
-	            if (!isset($_SESSION["counter"])) $_SESSION["counter"] = 0;
-	            $_SESSION["counter"]++;
+				$_SESSION[$_CONF['CAuthSessionName']]['username'] = $_POST['username'];
+	            $_SESSION[$_CONF['CAuthSessionName']]['password'] = md5($_POST['password']);
+	            $_SESSION[$_CONF['CAuthSessionName']]['uid'] = $user['uid'];
+	            $_SESSION[$_CONF['CAuthSessionName']]["remote_addr"]  = $_SERVER["REMOTE_ADDR"];
+	            $_SESSION[$_CONF['CAuthSessionName']]["session_name"] = session_name();
+	            $_SESSION[$_CONF['CAuthSessionName']]["user_agent"]   = $_SERVER["HTTP_USER_AGENT"];
+
+	            if (!isset($_SESSION["REMOTE_ADDR"])) {
+	                $_SESSION["REMOTE_ADDR"] = $_SERVER["REMOTE_ADDR"];
+                }
+
+	            if (!isset($_SESSION['counter'])) {
+	                $_SESSION['counter'] = 0;
+                }
+
+	            $_SESSION['counter']++;
 				// die("#".__LINE__." _SESSION:".print_r($_SESSION, true)."<br>\n");
-				if (isset($user["uid"]) && !empty($user["uid"])) {
+				if (isset($user['uid']) && !empty($user['uid'])) {
 					$m = __LINE__;
 					// SETZE ONLINE-STATUS => LOGIN
-					set_lastlogin($ConnUserDB["connid"], $_TABLE["user"], $user["uid"], SET_NEW_VISIT);
+					set_lastlogin($ConnUserDB['connid'], $_TABLE['user'], $user['uid'], SET_NEW_VISIT);
 				}
 				
 				// COOKIES UEBER LOGIN-BRUECKE INITIALISIEREN
-				if (!strchr($redirect,"?")) $redirect.="?";
-				$ausgabe=implode("",file(PATH_TO_LOGIN_MODUL."html/login_bridge.html"));
-				$ausgabe=str_replace("%redirect%", $redirect, $ausgabe);
-				$ausgabe=str_replace("%name%", $user["vorname"]." ".$user["nachname"], $ausgabe);
+				if (strpos($redirect,'?') === false || strpos($redirect, '?area=start') !== false) {
+                    switch($user['gruppe']) {
+
+                        case "kunde_report":
+                            $redirect = '?s=Property';
+                            break;
+
+                        case "admin_standort":
+                        case "admin_gesamt":
+                        case "admin":
+                            $redirect = './?s=aantraege';
+                            break;
+
+                        case 'umzugsteam':
+                            // die('#' . __LINE__ . ' ' . __FILE__ . ' : ' . $user['gruppe']);
+                            $redirect = './?s=auslieferung';
+                            break;
+
+                        case 'user':
+                        default:
+                        $redirect = './index.php?s=Umzug';
+                            break;
+                    }
+                }
+				
+				$ausgabe = implode('',file(PATH_TO_LOGIN_MODUL . 'html/login_bridge.html'));
+				$ausgabe.= '<div>Redirect: ' . $redirect . '</div>';
+				$ausgabe = str_replace('%redirect%', $redirect, $ausgabe);
+				$ausgabe = str_replace('%name%', $user['vorname'] . ' ' . $user['nachname'], $ausgabe);
 				db_close();
 				clearstatcache();
 				$show_form = false;
